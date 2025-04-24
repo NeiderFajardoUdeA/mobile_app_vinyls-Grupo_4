e9295f87f18cc478a4bbcb04c91de96c2000a584
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2025-04-24 17:15:36 -0500

    Creamos los dos primeros escnarios de pruebas en el archivo AlbumTest

---
+++ app/src/androidTest/java/com/moviles/vinilos/AlbumTest.java
@@ -0,0 +1,50 @@
+package com.moviles.vinilos;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AlbumTest {
+    //Inicializamos la MainActivity antes de cada test
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void verListaAlbumes() {
+        //Given que estoy en el home de la app
+        ViewInteraction albumesBtn = AlbumsUtils.verBtnAlbums();
+
+        //When da clic en el boton "Albumes"
+        AlbumsUtils.clickBtn(albumesBtn);
+
+        //Then entra al listado de albumes ve el titulo de la vista
+        AlbumsUtils.validamosTituloVistaAlbumes();
+    }
+
+    @Test
+    public void verListaAlbumesYVolverHome() {
+        //Given que estoy en el home de la app
+        ViewInteraction albumesBtn = AlbumsUtils.verBtnAlbums();
+
+        //And da clic en el boton "Albumes"
+        AlbumsUtils.clickBtn(albumesBtn);
+
+        //And entra al listado de albumes ve el titulo de la vista
+        AlbumsUtils.validamosTituloVistaAlbumes();
+
+        //When da clic en el boton de menu
+        ViewInteraction menuBtn = AlbumsUtils.verBtnMenu();
+
+        //And da clic en el boton "Menu"
+        AlbumsUtils.clickBtn(menuBtn);
+
+        //Then deberia verse el la imagen del home
+        AlbumsUtils.validamosImagenHome();
+    }
+}
---
+++ app/src/androidTest/java/com/moviles/vinilos/AlbumsUtils.java
@@ -0,0 +1,33 @@
+package com.moviles.vinilos;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import androidx.test.espresso.ViewInteraction;
+
+public class AlbumsUtils {
+
+    public static ViewInteraction verBtnAlbums() {
+        return onView(allOf(withId(R.id.albumesButton), withText(R.string.boton_menu_albums), isDisplayed()));
+    }
+
+    public static void clickBtn(ViewInteraction btn) {
+        btn.perform(click());
+    }
+
+    public static void validamosTituloVistaAlbumes() {
+        onView(withId(R.id.albumsTitle)).check(matches(allOf(isDisplayed(), withText(R.string.boton_menu_albums))));
+    }
+
+    public static ViewInteraction verBtnMenu() {
+        return onView(allOf(withId(R.id.menuIcon), isDisplayed()));
+    }
+
+    public static void validamosImagenHome() {
+        onView(allOf(withId(R.id.logoImage), isDisplayed())).check(matches(isDisplayed()));
+    }
+}
